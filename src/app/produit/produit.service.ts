0613d8b9c79be59f1ff1dd9416626c6637090542
Your Name <you@example.com>
2018-11-06 15:03:53 +0000

    finishing dao

--- src/app/produit/produit.service.ts
+++ src/app/produit/produit.service.ts
@@ -6,10 +6,10 @@ export class ProduitService2 {
 
     PRODUITS: Produit[]=[];
   constructor() { 
-    let p1= new Produit('livre',234,50);
-    let p2= new Produit('cahier',300,12.65);
-    let p3= new Produit('cartable',500,120);
-    let p4= new Produit('stylo',321,5);
+    let p1= new Produit(1,'livre',234,50);
+    let p2= new Produit(2,'cahier',300,12.65);
+    let p3= new Produit(3,'cartable',500,120);
+    let p4= new Produit(4,'stylo',321,5);
     this.PRODUITS.push(p1);
     this.PRODUITS.push(p2);
     this.PRODUITS.push(p3);
--- src/app/shared/produit.ts
+++ src/app/shared/produit.ts
@@ -1,7 +1,7 @@
 
 export class Produit{
 
-    constructor(public ref?: string ,public quantite?: number,public prix?: number,public id?: number)
+    constructor(public id?: number,public ref?: string ,public quantite?: number,public prix?: number)
     {
 
     }
